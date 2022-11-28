1500314cc9888aecf901c22217fb25a109776f8a
Thiago Marsal Farias <thiagomarsal.farias@gmail.com>
2022-11-27 19:38:50 -0700

    Update scanPorts.sh
    updates to scan IP range CIDR and validation.

--- scanPorts.sh
+++ scanPorts.sh
@@ -1,12 +1,23 @@
-#!bin/bash
+#!/bin/bash
 
 echo "Starting scan open ports for the IP Address: $1"
+len=`expr length "$1"`
 
-if [$1 == ""]
+#check if the param is empty. If there is no value it skip the process.
+if [ $len -eq 0 ];
 then
-	echo "Please inform the IP Address. For example: scanPorts.sh [IP] / scanPorts.sh 192.168.1.1"
-else
-	echo "The scan process can take several minutes to complete."
-	nc -nvz $1 1-65535
+   echo "Please inform the IP Address. For example: scanPorts.sh [IP] / scanPorts.sh 192.168.1.1 OR scanPorts.sh 192.168.1.1/24"
+   exit
 fi
-	echo "The scanning process has finisehd."
+
+#Otherwise, the scan will be executed for the ip address passed by param.
+if [[ "$1" == *"/"* ]];
+then
+# Check for range ip address
+  nmap -sV $1
+else
+# Check for single ip address
+  echo "The scan process can take several minutes to complete."
+  nc -nvz $1 1-65535
+fi
+echo "The scanning process has finisehd."
